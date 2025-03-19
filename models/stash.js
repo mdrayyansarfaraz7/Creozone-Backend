453ff2341321d66616d5c7e9a83305ed537b979b
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-03-19 19:03:35 +0530

    stash model created

---
+++ models/stash.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+
+const stashSchema = mongoose.Schema({
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    creations: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Creation'
+        }
+    ],
+    styleChain: [
+        {
+            designer: {
+                type: mongoose.Types.ObjectId,
+                ref: 'User'
+            },
+            role: {
+                type: String,
+                enum: ['creator', 'contributor'],
+                default: 'creator'
+            }
+        }
+    ],
+    Resculpt: [
+        {
+            resculpter: {
+                type: mongoose.Types.ObjectId,
+                ref: 'User'
+            },
+            imgUrl: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String,
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    likes: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, { timestamps: true });
+
+export default mongoose.model('Stash', stashSchema);
