1de34cf6b5c4b9770a9c925428aba123de732bbe
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-05-15 13:41:00 +0530

    updated view creations route controller

--- controllers/creationController.js
+++ controllers/creationController.js
@@ -2,10 +2,12 @@ import creation from "../models/creation.js";
 
 export const findCreation=async(req,res)=>{
     const {id}=req.params;
+    console.log(id);
     try {
-      const creationDetails=await creation.findOne({id});
-      if(!stashDetails){
-    res.status(401).send({message:"Creation Not found found"});
+      const creationDetails = await creation.findById(id).populate({path:'author',select:'username email avatar'}).populate({path:'stash',select:'title'});
+      console.log(creationDetails);
+      if(!creationDetails){
+    res.status(401).send({message:"Creation Not  found"});
       }
       else{
     res.send({message:"Creaton found",creationDetails});
@@ -13,6 +15,6 @@ export const findCreation=async(req,res)=>{
     } catch (error) 
     {
       console.log(error);
-      res.status(401).send({message:"Creation Not found found"});
+      res.status(401).send({message:"Something went wrong"});
     }
 }
