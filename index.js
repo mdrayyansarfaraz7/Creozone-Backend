773acc19de366ac2d0dba9f169ed3015c3ce4872
Md Rayyan Sarfaraz <mdrayyansarfaraz@gmail.com>
2025-10-06 20:59:44 +0530

    cors origin added

--- index.js
+++ index.js
@@ -23,10 +23,23 @@ connectDB();
 
 const app=express();
 
+const allowedOrigins = [
+  'http://localhost:5173',             // local dev
+  'https://creozone.vercel.app'       // deployed frontend
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173', 
-    credentials: true               
-  }));
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow non-browser requests like Postman
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
 
 app.use(express.json());
 
